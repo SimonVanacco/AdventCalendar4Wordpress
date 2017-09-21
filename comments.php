7583a3b7878c09ae595253635176851c3f5a0b85
Sempre Vivo <thegalaxiasgroup@gmail.com>
2017-09-21 17:33:51 +0200

    Comment list style & iFrame Responsivness

--- comments.php
+++ comments.php
@@ -1,27 +1,47 @@
-<?php if ( post_password_required() ) {
-	return;
-} ?>
-	<div id="comments" class="comments-area">
-		<?php if ( have_comments() ) : ?>
-			<h3 class="comments-title">
-				<?php
-				printf( _nx( 'One comment on “%2$s”', '%1$s comments on “%2$s”', get_comments_number(), 'comments title'),
-					number_format_i18n( get_comments_number() ), get_the_title() );
-				?>
-			</h3>
-			<ul class="comment-list">
-				<?php
-				wp_list_comments( array(
-					'short_ping'  => true,
-					'avatar_size' => 50,
-				) );
-				?>
-			</ul>
-		<?php endif; ?>
-		<?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-			<p class="no-comments">
-				<?php _e( 'Comments are closed.' ); ?>
-			</p>
-		<?php endif; ?>
-		<?php comment_form(); ?>
-	</div>
+<aside id="comments" class="col-md-9">
+ <div class="card card-body">
+     <?php if ( have_comments() ) : ?>
+         <h2 class="comments-title">
+             <?php
+             $comments_number = get_comments_number();
+             if ( 1 === $comments_number ) {
+                 echo "1 commentaire sur cette case: réagissez à votre tour!";
+             } else {
+
+                 echo $comments_number." commentaires sur cette case";
+             }
+             ?>
+         </h2>
+
+         <?php the_comments_navigation(); ?>
+
+         <ol class="comment-list">
+             <?php
+             wp_list_comments( array(
+                 'style'       => 'ol',
+                 'short_ping'  => true,
+                 'avatar_size' => 42,
+             ) );
+             ?>
+         </ol><!-- .comment-list -->
+
+         <?php the_comments_navigation(); ?>
+
+     <?php endif; // Check for have_comments(). ?>
+
+     <?php
+     // If comments are closed and there are comments, let's leave a little note, shall we?
+     if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+         ?>
+         <p class="no-comments">Il n'y a pas encore de commentaires... Soyez le premier!</p>
+     <?php endif; ?>
+
+     <?php
+     comment_form( array(
+         'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+         'title_reply_after'  => '</h2>',
+     ) );
+     ?>
+
+ </div>
+</aside><!-- .comments-area -->
--- content-single.php
+++ content-single.php
@@ -1,7 +1,6 @@
 <article class="col-md-9">
     <div class="card">
         <div class="card-body">
-            <h4 class="card-title"><?php the_title(); ?></h4>
             <p class="card-text"><?php the_content(); ?></p>
         </div>
     </div>
